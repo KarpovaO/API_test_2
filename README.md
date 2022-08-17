f967459ddcf9e523d47f18d79eabf22db0ca2bcd
KarpovaO <grose@yandex.ru>
2022-08-17 22:23:43 +0300

    POST

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/kiqgr3lix6fnae89/branch/master?svg=true)](https://ci.appveyor.com/project/KarpovaO/api-test/branch/master)
---
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,29 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldCheckBodyAndHead() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("something, no matter what")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("something, no matter what"))
+                .body("url", equalTo("https://postman-echo.com/post"))
+                .header("Content-Type", "application/json; charset=utf-8")
+                .header("Vary", "Accept-Encoding")
+        ;
+    }
+}
